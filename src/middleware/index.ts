7249f0d48541b1784a105da3cea1f6f28b23705f
Felipe Vasconcelos <lipevasconcelos20@gmail.com>
2024-06-07 12:29:33 -0300

    criação do middleware para tratamento de erros

---
+++ src/middleware/index.ts
@@ -0,0 +1,6 @@
+import requestHandler from './requestHandler';
+import HttpException from './httpException';
+import errorMiddleware from './errormiddleware';
+
+
+export { requestHandler, HttpException, errorMiddleware };
