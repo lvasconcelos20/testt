bff6d0dbb13c79232dea7dd36c14ae1785073273
Felipe Vasconcelos <lipevasconcelos20@gmail.com>
2024-06-08 17:17:58 -0300

    instalação dos pacotes

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ac07221a87ee9bf"; }
+/******/ 		__webpack_require__.h = function() { return "56ec6dca49705370"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
