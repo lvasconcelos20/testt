c9efcb79da688d5c2e85b0c13348f17c26b7ff30
Felipe Vasconcelos <lipevasconcelos20@gmail.com>
2024-06-10 01:20:23 -0300

    config

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "174866791ff451c1"; }
+/******/ 		__webpack_require__.h = function() { return "17c74aa3b7bd050c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
